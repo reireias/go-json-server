9a7200c6b5592ff665a75528c6a16729395a0c17
reireias <reireiAs@gmail.com>
2017-10-22 00:07:04 +0900

    add base code.

---
+++ jserver.go
@@ -0,0 +1,33 @@
+package jserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type JsonRouter struct {
+	Router *mux.Router
+}
+
+func (r *JsonRouter) Add(path string, file string, x interface{}) error {
+	raw, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(raw, &x)
+	r.Router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		buff, _ := json.Marshal(x)
+		fmt.Fprintf(w, string(buff))
+	})
+	return nil
+}
+
+func NewJsonRouter() *JsonRouter {
+	router := &JsonRouter{}
+	router.Router = mux.NewRouter()
+	return router
+}
---
+++ jserver_test.go
@@ -0,0 +1,48 @@
+package jserver
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type Sample struct {
+	ID   int    `json:"id"`
+	Age  int    `json:"age"`
+	Name string `json:"name"`
+}
+
+func TestPath(t *testing.T) {
+	type Sample struct {
+		ID   int    `json:"id"`
+		Age  int    `json:"age"`
+		Name string `json:"name"`
+	}
+
+	var s []Sample
+	jsonRouter := NewJsonRouter()
+	jsonRouter.Add("/test", "./test.json", s)
+
+	ts := httptest.NewServer(jsonRouter.Router)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/test")
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
+
+	fmt.Println(string(body))
+	if string(body) != `[{"age":14,"id":1,"name":"kaban"},{"age":15,"id":2,"name":"serval"}]` {
+		t.Error("response body invalid")
+		return
+	}
+}
