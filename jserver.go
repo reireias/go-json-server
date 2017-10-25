01c459dc8acda986667b6ee34161c61d96a4bb65
reireias <reireiAs@gmail.com>
2017-10-25 21:52:08 +0900

    add id path.

--- jserver.go
+++ jserver.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/koron/go-dproxy"
 )
 
 type JsonRouter struct {
@@ -21,7 +22,23 @@ func (r *JsonRouter) Add(path string, file string, x interface{}) error {
 	json.Unmarshal(raw, &x)
 	r.Router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		buff, _ := json.Marshal(x)
+		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, string(buff))
+	})
+	r.Router.HandleFunc(path+"/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		targetID := vars["id"]
+		w.WriteHeader(http.StatusOK)
+		for _, v := range x.([]interface{}) {
+			id, _ := dproxy.New(v).M("id").String()
+			if targetID == id {
+				buff, _ := json.Marshal(v)
+				fmt.Fprintf(w, string(buff))
+				return
+			}
+		}
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "{\"error\":\"Not Found.\"}")
 	})
 	return nil
 }
--- jserver_test.go
+++ jserver_test.go
@@ -7,15 +7,9 @@ import (
 	"testing"
 )
 
-type Sample struct {
-	ID   int    `json:"id"`
-	Age  int    `json:"age"`
-	Name string `json:"name"`
-}
-
 func TestPath(t *testing.T) {
 	type Sample struct {
-		ID   int    `json:"id"`
+		ID   string `json:"id"`
 		Age  int    `json:"age"`
 		Name string `json:"name"`
 	}
@@ -39,7 +33,39 @@ func TestPath(t *testing.T) {
 		return
 	}
 
-	if string(body) != `[{"age":14,"id":1,"name":"kaban"},{"age":15,"id":2,"name":"serval"}]` {
+	if string(body) != `[{"age":14,"id":"1","name":"kaban"},{"age":15,"id":"2","name":"serval"}]` {
+		t.Error("response body invalid")
+		return
+	}
+}
+
+func TestID(t *testing.T) {
+	type Sample struct {
+		ID   string `json:"id"`
+		Age  int    `json:"age"`
+		Name string `json:"name"`
+	}
+
+	var s []Sample
+	jsonRouter := NewJsonRouter()
+	jsonRouter.Add("/test", "./test.json", s)
+
+	ts := httptest.NewServer(jsonRouter)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/test/1")
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error("unexpected")
+		return
+	}
+
+	if string(body) != `{"age":14,"id":"1","name":"kaban"}` {
 		t.Error("response body invalid")
 		return
 	}
